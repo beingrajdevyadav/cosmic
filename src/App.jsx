e7da154edceb598198b935c0e9d4a94877709170
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-16 23:37:24 +0530

    cosmic form created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CosmicForm from './components/CosmicForm'
 import Footer from './components/Footer'
 import Header from './components/Header'
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Header />
-
+      <CosmicForm />
       <Footer />
     </>
   )
---
+++ src/components/CosmicForm.jsx
@@ -0,0 +1,40 @@
+import React, { useRef, useState } from 'react'
+
+const CosmicForm = () => {
+    const [name, setName] = useState("");
+    const [birthDate, setBirthDate] = useState("DOB");
+
+    const dataRef = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("form submitted!")
+    }
+
+    const handleDateButtonClick = () => {
+        dataRef.current.showPicker?.();
+        dataRef.current.click();
+        console.log("data button clicked!");
+    }
+    return (
+        <div>
+            <form action="" onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <input type="text" placeholder='Enter your fullname' value={name} onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div className="form-control">
+                    <input style={{display:"none"}}  type="date" value={birthDate} placeholder='Enter your fullname' ref={dataRef} onChange={(e) => { setBirthDate(e.target.value) }} />
+
+                    <span>{birthDate}</span> : <button type='button' onClick={handleDateButtonClick}>Click to pick</button>
+                </div>
+
+
+                <div className="btn-control">
+                    <button type='submit'>Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default CosmicForm
