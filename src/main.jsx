f746cac5dd84e3c468af264103323a2c782fdd75
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-17 21:40:17 +0530

    VideoBackground Component Created.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import VideoBackground from './components/VideoBackground.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <VideoBackground/>
   </StrictMode>,
 )
